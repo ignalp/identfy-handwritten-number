11eb8a1b32f11d13d255ec0f1a07e69ee134efee
Ignacio Lopez <ignalp@gmail.com>
2021-02-04 12:40:46 -0600

    Updated to use streamlit_drawable_canvas==0.6.0

--- cv_web_app.py
+++ cv_web_app.py
@@ -3,24 +3,46 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import numpy as np
 import cv2
+import time
 from utils import *
 
-def prediction(net):   
-    # Draw or clear?
-    drawing_mode = st.checkbox("Draw or clear?",True)
 
-    # Create a canvas component
-    image_data = st_canvas(
-        15, '#FFF', '#000', height=280,width=280, drawing_mode=drawing_mode, key="canvas"
+def prediction(net):   
+
+    # Specify canvas parameters in application
+    stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+    stroke_color = st.sidebar.color_picker("Stroke color hex: ","#FFFFFF")
+    bg_color = st.sidebar.color_picker("Background color hex: ", "#000")
+    bg_image = None
+    drawing_mode = "freedraw"
+    #realtime_update = st.sidebar.checkbox("Update in realtime", True)
+    realtime_update =  True
+
+# Create a canvas component
+    canvas_result = st_canvas(
+        fill_color="rgba(0, 0, 0, 1)",  # Fixed fill color with some opacity
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        background_color="" if bg_image else bg_color,
+        background_image=Image.open(bg_image) if bg_image else None,
+        update_streamlit=realtime_update,
+        height=280, width=280,
+        drawing_mode=drawing_mode,
+        key="canvas",
     )
 
+    digit = 0 
+    confidence = 0 
+
     # Predicting the image
-    if image_data is not None:
+    if canvas_result.image_data is not None:
         if st.button('Predict'):
             # Model inference
-            digit, confidence = predictDigit(image_data,net)
-            st.write('Recognized Digit: {}'.format(digit))
-            st.write('Confidence: {:.2f}'.format(confidence))
+            digit, confidence = predictDigit(canvas_result.image_data,net)
+    
+
+    return digit, confidence
+
 
 def main():
     # Load Digit Recognition model
@@ -30,10 +52,15 @@ def main():
     st.write("\n\n")
     st.write("Draw a digit below and click on Predict button")
     st.write("\n")
-    st.write("To clear the digit, uncheck checkbox, double click on the digit or refresh the page")
-    st.write("To draw the digit, check the checkbox")
+    st.write("To clear the digit, click the trash bin icon below the image")
+    
+    digit, confidence = prediction(net)
 
-    prediction(net)
+    st.write('Recognized Digit: {}'.format(digit))
+    st.write('Confidence: {:.2f}'.format(confidence))
+
+    
+
 
 if __name__ == '__main__':
     main()
